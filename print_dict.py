a52763d933e59a650ce93da2aceefbda2bdc82cc
ArturTaerov <103738494+ArturTaerov@users.noreply.github.com>
2022-06-11 22:31:08 +0300

    print_dict

---
+++ print_dict.py
@@ -0,0 +1,16 @@
+dict_print = {
+    'склад': 
+        {'HP LaserJet': ['100', 'принтер', 'high', 'сетевой'], 'Xerox WorkCentre': ['200', 'ксерокс', 'medium', 'А4']}, 
+    'адм': 
+        {'Xerox WorkCentre': ['300', 'ксерокс', 'medium', 'А4'], 'HP ScanJet': ['400', 'сканер', 'medium', '600dpi']}, 
+    'отдел': 
+        {'HP LaserJet': ['500', 'принтер', 'high', 'сетевой']}, 'Xerox WorkCentre': ['600', 'ксерокс', 'medium', 'А4']
+    }
+
+j = 0
+for i in dict_print:
+    j+=1
+    print(f"{j}-я итерация: j = ", j)
+    print("i = ", i)
+    print("i = ", dict_print.get(i))
+
